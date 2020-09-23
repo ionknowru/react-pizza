b2998c07b7c264ebdfe7beccb8f113c8b8cc855c
Nikitoon <nyajin.nyakin@yandex.ru>
2020-09-23 11:45:46 +0300

    Created new page and added some features

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react'
 
-function Categories({ items, onClickItem }) {
+function Categories({ items }) {
     const [activeItem, setActiveItem] = useState(null);
 
     const onSelectItem = (index) => {
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header/Header';
 import Button from './Button/Button';
 import Categories from './Categories/Categories';
+import Popup from './Popup/Popup';
 
-export {Header, Button, Categories};
+export {Header, Button, Categories, Popup};
